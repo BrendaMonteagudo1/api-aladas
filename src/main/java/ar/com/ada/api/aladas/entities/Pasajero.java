53efe397bd236dbd00925bfd8886d83d89570ca5
BrendaMonteagudo1 <brendamonteagudo@gmail.com>
2021-07-20 21:09:34 -0300

    se generan entities (aeropuerto - pais -pasajero - persona - reserva- staff - vuelo) con sus respectivas variables y notaciones //  la entiti pais cuenta con enumerados para el registro de paises y otro para dni o pasaporte

---
+++ src/main/java/ar/com/ada/api/aladas/entities/Pasajero.java
@@ -0,0 +1,33 @@
+package ar.com.ada.api.aladas.entities;
+
+import java.util.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "pasajero")
+public class Pasajero extends Persona {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pasajero_id")
+    private Integer pasajeroId;
+
+    @OneToMany(mappedBy = "pasajero", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Reserva> reservas = new ArrayList<>();
+
+    //getters y setters
+
+    public Integer getPasajeroId() {
+        return pasajeroId;
+    }
+
+    public void setPasajeroId(Integer pasajeroId) {
+        this.pasajeroId = pasajeroId;
+    }
+
+    public void agregarReserva(Reserva reserva) {
+        this.reservas.add(reserva);
+        reserva.setPasajero(this); //relacion bidireccional
+    }
+
+}
---
+++ src/main/java/ar/com/ada/api/aladas/entities/Staff.java
@@ -0,0 +1,24 @@
+package ar.com.ada.api.aladas.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "staff")
+public class Staff extends Persona {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "staff_id")
+    private Integer staffId;
+
+    //getters y setters
+
+    public Integer getStaffId() {
+        return staffId;
+    }
+
+    public void setStaffId(Integer staffId) {
+        this.staffId = staffId;
+    }
+
+}
