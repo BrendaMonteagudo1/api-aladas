c545e7762f670ef8291cb389a68f6c934294bc9d
BrendaMonteagudo1 <brendamonteagudo@gmail.com>
2021-07-27 21:00:04 -0300

    se generan dos test de validacion de precio

--- src/test/java/ar/com/ada/api/aladas/AladasApplicationTests.java
+++ src/test/java/ar/com/ada/api/aladas/AladasApplicationTests.java
@@ -1,7 +1,16 @@
 package ar.com.ada.api.aladas;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.math.BigDecimal;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import ar.com.ada.api.aladas.Service.VueloService;
+import ar.com.ada.api.aladas.entities.Vuelo;
 
 @SpringBootTest
 class AladasApplicationTests {
@@ -9,5 +18,35 @@ class AladasApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	@Autowired
+	VueloService vueloService;
+
+	@Test
+	void vueloTestPrecioNegativo() {
+
+		Vuelo vueloConPrecioNegativo = new Vuelo();
+		vueloConPrecioNegativo.setPrecio(new BigDecimal(-100));
+
+		// Assert: afirmar
+		// afirmar quie sea verdadero: assertFalse
+		assertFalse(vueloService.validarPrecio(vueloConPrecioNegativo));
+
+	}
+
+	@Test
+	void vueloTestPrecioOk() {
+
+		Vuelo vueloConPrecioOK = new Vuelo();
+		vueloConPrecioOK.setPrecio(new BigDecimal(100));
+
+		// Assert: afirmar
+		// afirmar quie sea verdadero: assertTrue
+		assertTrue(vueloService.validarPrecio(vueloConPrecioOK));
+
+	}
 
 }
+
+
+
