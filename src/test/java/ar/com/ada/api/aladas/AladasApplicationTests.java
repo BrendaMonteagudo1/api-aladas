3b6fa1469a862f5ccfc39a9631b3218c74de9ea8
BrendaMonteagudo1 <brendamonteagudo@gmail.com>
2021-07-29 20:49:33 -0300

    se generan test unit para aeropuertoValidarCodigoIATA y se llama a aeropuerto service /  dentro de aeropuerto service se genera el metodo validarCodigoIATA

--- src/test/java/ar/com/ada/api/aladas/AladasApplicationTests.java
+++ src/test/java/ar/com/ada/api/aladas/AladasApplicationTests.java
@@ -9,7 +9,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import ar.com.ada.api.aladas.Service.AeropuertoService;
 import ar.com.ada.api.aladas.Service.VueloService;
+import ar.com.ada.api.aladas.entities.Aeropuerto;
 import ar.com.ada.api.aladas.entities.Vuelo;
 
 @SpringBootTest
@@ -21,6 +23,9 @@ class AladasApplicationTests {
 	
 	@Autowired
 	VueloService vueloService;
+
+	@Autowired
+	AeropuertoService aeropuertoService;
 
 	@Test
 	void vueloTestPrecioNegativo() {
@@ -45,8 +50,96 @@ class AladasApplicationTests {
 		assertTrue(vueloService.validarPrecio(vueloConPrecioOK));
 
 	}
+	@Test
+	void aeropuertoValidarCodigoIATAOK() {
+		// From Florencia Di Felice to Everyone: 07:42 PM
+		// el código no debe llevar número y sólo 3 letras, así que habría que limitarlo
+		// a eso, no?
+
+		String codigoIATAOk1 = "EZE";
+		String codigoIATAOk2 = "AEP";
+		String codigoIATAOk3 = "NQN";
+		String codigoIATAOk4 = "N  ";
+		String codigoIATAOk5 = "N39";
+
+		/*//String codigoIATAOk4 = "N  ";
+
+		//En este caso, afirmo que espero que el length del codigoIATAOk1 sea 3
+		assertEquals(3, codigoIATAOk1.length());
+
+		//En este caso, afirmo que espero qeu el resultado de la condicion
+		//sea verdaderro(en este caso, lenght == 3)
+		assertTrue(codigoIATAOk2.length() == 3);
+
+		//assertTrue(codigoIATAOk4.length() == 3);*/
+
+		Aeropuerto aeropuerto1 = new Aeropuerto();
+		aeropuerto1.setCodigoIATA(codigoIATAOk1);
+
+		Aeropuerto aeropuerto2 = new Aeropuerto();
+		aeropuerto2.setCodigoIATA(codigoIATAOk2);
+
+		Aeropuerto aeropuerto3= new Aeropuerto();
+		aeropuerto3.setCodigoIATA(codigoIATAOk3);
+
+		
+		Aeropuerto aeropuerto4= new Aeropuerto();
+		aeropuerto4.setCodigoIATA(codigoIATAOk4);
+
+		assertTrue(aeropuertoService.validarCodigoIATA(aeropuerto1));
+		assertTrue(aeropuertoService.validarCodigoIATA(aeropuerto2));
+		assertTrue(aeropuertoService.validarCodigoIATA(aeropuerto3));
+
+
+		assertFalse(aeropuertoService.validarCodigoIATA(aeropuerto4));
+
+		
+	}
+
+	@Test
+	void aeropuertoValidarCodigoIATANoOK() {
+		// From Florencia Di Felice to Everyone: 07:42 PM
+		// el código no debe llevar número y sólo 3 letras, así que habría que limitarlo
+		// a eso, no?
+
+	}
+
+	@Test
+	void vueloVerificarValidacionAeropuertoOrigenDestino() {
+		// En este validar todas las posibilidades de si el aeropuerto
+		// origen es igual al de destion o todo lo que se les ocurra
+	}
+
+	@Test
+	void vueloChequearQueLosPendientesNoTenganVuelosViejos() {
+
+		// Queremos validar que cuando hagamos un metodo que traiga los vuelos actuales
+		// para
+		// hacer reservas, no haya ningun vuelo en el pasado.
+	}
+
+	@Test
+	void vueloVerificarCapacidadMinima() {
+
+	}
+
+	@Test
+	void vueloVerificarCapadidadMaxima() {
+
+	}
+
+	@Test
+	void aeropuertoTestBuscadorIATA() {
+
+	}
 
 }
+
+
+
+
+
+
 
 
 
