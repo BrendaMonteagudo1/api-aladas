249ffbc57c54fceaf47d8fb1a5637e8ef4fc7f84
BrendaMonteagudo1 <brendamonteagudo@gmail.com>
2021-08-15 14:03:11 -0300

    se generan pasajero repositorio y service , para ser utilizado en pasaje, ya que cuando se genera un pasaje cuenta con un pasajero, en dicha recerva y repo se encuentran los metodos find by id  crear pasajero

---
+++ src/main/java/ar/com/ada/api/aladas/Service/PasajeroService.java
@@ -0,0 +1,23 @@
+package ar.com.ada.api.aladas.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ar.com.ada.api.aladas.repos.PasajeroRepository;
+import ar.com.ada.api.aladas.entities.*;
+
+@Service
+public class PasajeroService {
+    @Autowired
+    PasajeroRepository repo;
+
+    public void crearPasajero(Pasajero pasajero) {
+        repo.save(pasajero);
+
+    }
+
+    public Pasajero buscarPorId(Integer id) {
+        return repo.findByPasajeroId(id);
+    }
+
+}
