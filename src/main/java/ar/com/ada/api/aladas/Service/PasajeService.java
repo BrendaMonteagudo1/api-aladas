581710234c0f5321a0a76bb92f1aa1d3195585fb
BrendaMonteagudo1 <brendamonteagudo@gmail.com>
2021-08-17 18:49:56 -0300

    se genera en pasaje un if de validacon de capacidad para cambiar el estado de vuelo a cerrado en caso de que la capacidad sea = a 0

--- src/main/java/ar/com/ada/api/aladas/Service/PasajeService.java
+++ src/main/java/ar/com/ada/api/aladas/Service/PasajeService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import ar.com.ada.api.aladas.entities.Pasaje;
 import ar.com.ada.api.aladas.entities.Reserva;
 import ar.com.ada.api.aladas.entities.Reserva.EstadoReservaEnum;
+import ar.com.ada.api.aladas.entities.Vuelo.EstadoVueloEnum;
 import ar.com.ada.api.aladas.repos.PasajeRepository;
 
 @Service
@@ -32,6 +33,12 @@ public class PasajeService {
         reserva.asociarPasaje(pasaje);
         Integer nuevaCapacidad = reserva.getVuelo().getCapacidad() - 1;
         reserva.getVuelo().setCapacidad(nuevaCapacidad);
+
+       if (nuevaCapacidad.equals(0)){
+          
+         reserva.getVuelo().setEstadoVueloId(EstadoVueloEnum.CERRADO);
+
+        }
 
         /* problema concurrencia
          * "update vuelo set capacidad = 29 where vueloid = 99 and capacidad = 30"
